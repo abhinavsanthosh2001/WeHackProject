c68db04de6612af4292838c14103bc66e591abf6
soju <sojung3149@gmail.com>
2024-03-24 10:34:05 -0500

    Checklist

--- 02-frontend/library-app/src/layouts/ChecklistPage/components/Checkbox.tsx
+++ 02-frontend/library-app/src/layouts/ChecklistPage/components/Checkbox.tsx
@@ -4,9 +4,21 @@ import React, { useState } from 'react';
 export const Checkbox =()=>{
     
       const [checkboxes, setCheckboxes] = useState([
-        { id: 1, label: 'Option 1', checked: false },
-        { id: 2, label: 'Option 2', checked: false },
-        { id: 3, label: 'Option 3', checked: false },
+        { id: 1, label: 'Accept Admission Offer', checked: false },
+        { id: 2, label: 'Verify Admitted Major: Check your admitted major as listed in your admissions acceptance letter and academics tab in Galaxy.', checked: false },
+        { id: 3, label: 'Register for Freshman Orientation', checked: false },
+        { id: 4, label: 'ALEKS Math Placement Assessment Exam OR college level math credits earned while in high school', checked: false },
+        { id: 5, label: 'Review and resolve any holds on your account', checked: false },
+        { id: 6, label: 'Optional Computer Science Placement Test: ', checked: false },
+
+        /*{ id: 1, label: 'Calculus (AB or BC)', checked: false },
+        { id: 2, label: 'Physics C Mechanics', checked: false },
+        { id: 3, label: 'Physics C Electromagnetism and Waves', checked: false },
+        { id: 4, label: 'Computer Science A', checked: false },
+        { id: 5, label: 'Applying for Admission', checked: false },
+        { id: 6, label: 'Freshman Orientation', checked: false },
+        { id: 7, label: 'Math Placement: The ALEKS Assessment OR college level math credits earned while in high school.', checked: false },*/
+        
         // Add more options as needed
     ]);
 
@@ -20,15 +32,17 @@ export const Checkbox =()=>{
 
     return (
         <div>
+          <h2>Freshman Checklist</h2>
             {checkboxes.map(checkbox => (
-                <div key={checkbox.id}>
+                <div key={checkbox.id} style={{ marginLeft: '50px', marginBottom: '10px' }}>
+                  
                     <input
                         type="checkbox"
                         id={checkbox.id+""}
                         checked={checkbox.checked}
                         onChange={() => handleCheckboxChange(checkbox.id)}
                     />
-                    <label htmlFor={checkbox.id+""}>{checkbox.label}</label>
+                    <label htmlFor={checkbox.id + ""} style={{ marginLeft: '5px', fontSize: '18px' }}>{checkbox.label}</label>
                 </div>
             ))}
         </div>
